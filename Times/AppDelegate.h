c45943cea98b715d74e2fcd532cf6897cba9c559
Doug Black <dougblack@gatech.edu>
2012-12-23 21:39:42 -0500

    Initial Commit

---
+++ Times/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Times
+//
+//  Created by Douglas Black on 12/23/12.
+//  Copyright (c) 2012 Doug Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Times/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Times
+//
+//  Created by Douglas Black on 12/23/12.
+//  Copyright (c) 2012 Doug Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
